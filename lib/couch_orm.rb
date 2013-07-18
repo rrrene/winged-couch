e8e9f31aab9c2aaae626e6249c849474de4c7d47
Ilya Bylich <ilya.bylich@productmadness.com>
2013-07-18 18:27:02 +0300

    Added Attributes module.

--- lib/couch_orm.rb
+++ lib/couch_orm.rb
@@ -2,6 +2,7 @@ require 'rest-client'
 require "couch_orm/version"
 require "couch_orm/http"
 require "couch_orm/configuration"
+require "couch_orm/model"
 
 # Main module
 #
---
+++ spec/couch_orm/model/attributes_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe CouchORM::Models::Attributes do
+
+  it ".attribute_names" do
+    SimpleModel.attribute_names.should eq [:name, :gender, :number, :unsupported]
+  end
+
+  it "#attributes" do
+    s = SimpleModel.new
+    s.name = "name"
+    s.gender = "gender"
+    s.number = "123"
+    expected_attributes = { name: "name",
+      gender: :gender,
+      number: 123,
+      unsupported: nil
+    }
+    s.attributes.should eq expected_attributes
+  end
+
+  describe "#attribute" do
+    it "default value" do
+      s = SimpleModel.new
+      s.gender.should eq "male"
+    end
+
+    it "raises error if class for type-casting is not supported" do
+      expect { SimpleModel.new.unsupported = nil }.to raise_error CouchORM::UnsupportedType
+    end
+  end
+
+end
