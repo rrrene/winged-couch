2b9c77387051660493a488c9a250946ded700a5c
Ilya Bylich <ilya.bylich@productmadness.com>
2013-08-07 15:33:10 +0300

    Moved javascripts to separated folder.

--- lib/couch_orm.rb
+++ lib/couch_orm.rb
@@ -1,14 +1,18 @@
-require 'rest-client'
-require "couch_orm/version"
-require "couch_orm/http"
 require "couch_orm/configuration"
-require "couch_orm/database"
-require "couch_orm/view"
-require "couch_orm/model"
-require 'couch_orm/views_loader'
 
 # Main module
 #
 module CouchORM
   extend Configuration
+
+  # @private
+  JAVASCRIPTS_PATH = File.expand_path("../couch_orm/javascripts", __FILE__)
 end
+
+require 'rest-client'
+require "couch_orm/version"
+require "couch_orm/http"
+require "couch_orm/database"
+require "couch_orm/view"
+require "couch_orm/model"
+require 'couch_orm/views_loader'
